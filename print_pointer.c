f5fc4719b4a013706323f4fe75d8c3dbf05becc1
0lston <figuigfree1@gmail.com>
2023-08-26 01:23:58 +0100

    fixed va_arg type

--- print_pointer.c
+++ print_pointer.c
@@ -10,7 +10,7 @@
 
 int print_p(va_list args)
 {
-	unsigned long int addr = (unsigned long int)va_arg(args, void *);
+	unsigned long int addr = va_arg(args, unsigned long int);
 	char *hex = convert(addr, 16);
 	int printed = 0;
 
